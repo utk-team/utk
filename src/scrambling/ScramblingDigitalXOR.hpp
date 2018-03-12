c634b0b741ac79bdd6b9a582a8778a2939bc84a8
Helene Perrier <perrier.ln@gmail.com>
2018-03-12 17:08:35 +0100

    minor

--- src/scrambling/ScramblingDigitalXOR.hpp
+++ src/scrambling/ScramblingDigitalXOR.hpp
@@ -49,6 +49,33 @@ public:
 		return true;
 	}
 	
+	template<unsigned int D, typename T, typename P>
+	bool scramble(const Pointset< D, T, P >& pts_input, Pointset< D, T, P >& pts_output, Vector<D, unsigned int> randvector)
+	{
+		if(pts_input.toricity!=1)
+			WARNING("ScramblingDigitalXOR:compute applying on a non-toric point set");
+		if (isFloatingType<T>())
+			WARNING("ScramblingDigitalXOR:compute applying on floating type coordinates. Clamping to integer values.");
+		
+		pts_output.resize(pts_input.size());
+		for(uint d=0; d<D; d++)
+		{
+			pts_output.domain.pMin.pos()[d] = pts_input.domain.pMin.pos()[d];
+			pts_output.domain.pMax.pos()[d] = pts_input.domain.pMax.pos()[d];
+		}
+		pts_output.toricity=pts_input.toricity;
+		
+		for(uint i=0; i<pts_input.size(); i++)
+		{
+			for(uint d=0; d<D; d++)
+			{
+				pts_output[i].pos()[d] = (unsigned int)(pts_input[i].pos()[d]) ^ (unsigned int)(randvector[d]);
+			}
+		}
+		
+		return true;
+	}
+	
 	
 protected:
 	std::mt19937 m_mersenneTwister;
