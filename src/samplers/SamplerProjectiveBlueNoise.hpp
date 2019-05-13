d98fcc0c1bd5233e68088ec1432feb6711249dc9
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2019-05-13 17:42:45 +0200

    Projective Blue Noise 6d

--- src/samplers/SamplerProjectiveBlueNoise.hpp
+++ src/samplers/SamplerProjectiveBlueNoise.hpp
@@ -58,8 +58,11 @@ namespace utk
 			double distDT_2D = sqrt(1.0/(2.0*sqrt(3)*arg_points));
 			double distDT_3D = pow(1.0/(4.0*sqrt(2)*arg_points), 1.0/3.0);
 			double distDT_4D = pow(1.0/(8.0*arg_points), 1.0/4.0);
-      double distDT_5D = 12.0;//pow(1.0/(4.0*sqrt(2)*arg_points), 1.0/3.0);
-      double distDT_6D = 12.0;//pow(1.0/(8.0*arg_points), 1.0/4.0);
+
+      double eta_5d =  1.0/30.0*M_PI*M_PI*sqrt(2.0);
+      double eta_6d =  1.0/144.0*M_PI*M_PI*M_PI*sqrt(3.0);
+      double distDT_5D = std::pow( 15.0*eta_5d / (8.0*M_PI*M_PI*arg_points), 1.0/5.0);
+      double distDT_6D = std::pow(  6.0*eta_6d / (M_PI*M_PI*M_PI*arg_points), 1.0/6.0);
       
 			std::cout << "Dists " << std::endl;
 			std::cout << "1D " << distDT_1D << std::endl;
@@ -129,6 +132,8 @@ namespace utk
 					//Id D=2, 01
 					//If D=3, 01, 12, 20
 					//If D=4, 01, 12, 20, 03, 13, 23
+          //If D=5, 01, 12, 20, 03, 13, 23, 04, 14, 24, 34
+          //If D=6, 01, 12, 20, 03, 13, 23, 04, 14, 24, 34, 05, 15, 25, 35, 45,
 					if(D>=2)
 					{
 						double current_dist = distDT_2D; // / dist_full;
@@ -202,7 +207,7 @@ namespace utk
 						}
 						
 					}
-					if(D==4)
+					if(D>=4)
 					{
 						for(int d=0; d<3; d++)
 						{
@@ -230,6 +235,64 @@ namespace utk
 									accept = false;
 							}
 						}
+          }
+          if(D>=5)
+            {
+              for(int d=0; d<4; d++)
+              {
+                double current_dist = distDT_2D;
+                current_dist *= ratio;
+                
+                for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
+                {
+                  double dist = 0;
+                  
+                  {
+                    double diff = std::abs((p.pos()[d]-arg_pts[j].pos()[d]));
+                    double toric_diff = std::min( diff, 1-diff );
+                    dist += toric_diff*toric_diff;
+                  }
+                  {
+                    double diff = std::abs((p.pos()[4]-arg_pts[j].pos()[4]));
+                    double toric_diff = std::min( diff, 1-diff );
+                    dist += toric_diff*toric_diff;
+                  }
+                  
+                  dist = sqrt(dist);
+                  
+                  if ( dist < current_dist )
+                  accept = false;
+                }
+              }
+            }
+          if(D==6)
+            {
+                for(int d=0; d<5; d++)
+                {
+                  double current_dist = distDT_2D;
+                  current_dist *= ratio;
+                  
+                  for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
+                  {
+                    double dist = 0;
+                    
+                    {
+                      double diff = std::abs((p.pos()[d]-arg_pts[j].pos()[d]));
+                      double toric_diff = std::min( diff, 1-diff );
+                      dist += toric_diff*toric_diff;
+                    }
+                    {
+                      double diff = std::abs((p.pos()[5]-arg_pts[j].pos()[5]));
+                      double toric_diff = std::min( diff, 1-diff );
+                      dist += toric_diff*toric_diff;
+                    }
+                    
+                    dist = sqrt(dist);
+                    
+                    if ( dist < current_dist )
+                    accept = false;
+                  }
+                }
 					}
 					
 					if(D==2)
@@ -238,7 +301,8 @@ namespace utk
 					//test 3D projections
 					//If D=3: 012
 					//If D=4: 012, 023, 013, 123
-					
+          //If D=5: 012, 023, 013, 123, 014, 024, 034, 124, 134, 234
+          //If D=6: 012, 023, 013, 123,
 					if(D>=3)
 					{
 						double current_dist = distDT_3D;
@@ -259,7 +323,7 @@ namespace utk
 								accept = false;
 						}
 					}
-					if(D==4)
+					if(D>=4)
 					{
 						for(int d=0; d<3; d++)
 						{
@@ -338,13 +402,141 @@ namespace utk
 						}
 	
 					}
+          if(D==5)
+          {
+            for(int d=0; d<4; d++)
+            {
+              double current_dist = distDT_3D;
+              current_dist *= ratio;
+              
+              for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
+              {
+                double dist = 0;
+                {
+                  double diff = std::abs((p.pos()[0]-arg_pts[j].pos()[0]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[1]-arg_pts[j].pos()[1]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[3]-arg_pts[j].pos()[4]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                dist = sqrt(dist);
+                
+                if ( dist < current_dist )
+                accept = false;
+              }
+              for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
+              {
+                double dist = 0;
+                {
+                  double diff = std::abs((p.pos()[0]-arg_pts[j].pos()[0]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[2]-arg_pts[j].pos()[2]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[3]-arg_pts[j].pos()[4]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                dist = sqrt(dist);
+                
+                if ( dist < current_dist )
+                accept = false;
+              }
+              for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
+              {
+                double dist = 0;
+                {
+                  double diff = std::abs((p.pos()[2]-arg_pts[j].pos()[1]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[1]-arg_pts[j].pos()[2]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[3]-arg_pts[j].pos()[4]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                dist = sqrt(dist);
+                
+                if ( dist < current_dist )
+                accept = false;
+              }
+              for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
+              {
+                double dist = 0;
+                {
+                  double diff = std::abs((p.pos()[2]-arg_pts[j].pos()[1]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[1]-arg_pts[j].pos()[3]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[3]-arg_pts[j].pos()[4]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                dist = sqrt(dist);
+                
+                if ( dist < current_dist )
+                accept = false;
+              }
+              for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
+              {
+                double dist = 0;
+                {
+                  double diff = std::abs((p.pos()[2]-arg_pts[j].pos()[2]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[1]-arg_pts[j].pos()[3]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                {
+                  double diff = std::abs((p.pos()[3]-arg_pts[j].pos()[4]));
+                  double toric_diff = std::min( diff, 1-diff );
+                  dist += toric_diff*toric_diff;
+                }
+                dist = sqrt(dist);
+                
+                if ( dist < current_dist )
+                accept = false;
+              }
+            }
+            
+          }
 					
 					if(D==3)
 						continue;
 
 					//test 4D projection
-					//If D=4: 0123
-					if(D==4)
+          //STOPPING HERE FOR NOW
+          //If D=4: 0123
+          //If D=5: 0123,
+          //If D=6: 0123,
+					if(D>=4)
 					{
 						double current_dist = distDT_4D;
 						current_dist *= ratio;
@@ -352,7 +544,7 @@ namespace utk
 						for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
 						{
 							double dist = 0;
-							for(uint dtmp=0; dtmp<D; dtmp++)
+							for(uint dtmp=0; dtmp<4; dtmp++)
 							{
 								double diff = std::abs((p.pos()[dtmp]-arg_pts[j].pos()[dtmp]));
 								double toric_diff = std::min( diff, 1-diff );
@@ -364,6 +556,46 @@ namespace utk
 								accept = false;
 						}
 					}
+          if(D>=5)
+          {
+            double current_dist = distDT_5D;
+            current_dist *= ratio;
+            
+            for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
+            {
+              double dist = 0;
+              for(uint dtmp=0; dtmp<5; dtmp++)
+              {
+                double diff = std::abs((p.pos()[dtmp]-arg_pts[j].pos()[dtmp]));
+                double toric_diff = std::min( diff, 1-diff );
+                dist += toric_diff*toric_diff;
+              }
+              dist = sqrt(dist);
+              
+              if ( dist < current_dist )
+              accept = false;
+            }
+          }
+          if(D==6)
+          {
+            double current_dist = distDT_6D;
+            current_dist *= ratio;
+            
+            for(uint j=0; (j<arg_pts.size()) & (accept); ++j)
+            {
+              double dist = 0;
+              for(uint dtmp=0; dtmp<D; dtmp++)
+              {
+                double diff = std::abs((p.pos()[dtmp]-arg_pts[j].pos()[dtmp]));
+                double toric_diff = std::min( diff, 1-diff );
+                dist += toric_diff*toric_diff;
+              }
+              dist = sqrt(dist);
+              
+              if ( dist < current_dist )
+              accept = false;
+            }
+          }
 				}
 				
 				arg_pts.push_back(p);
